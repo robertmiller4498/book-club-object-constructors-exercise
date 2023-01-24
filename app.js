f8a0a7210de339c5e4ff1c57feee0094b437fb96
robertmiller4498 <93550566+robertmiller4498@users.noreply.github.com>
2023-01-23 22:09:12 -0800

    Removed the for loop, repeating fixed.

--- app.js
+++ app.js
@@ -16,53 +16,24 @@ const bookOne = new Book("Barbarian Days", "William Finnegan", 447, false)
 const bookTwo = new Book("Kitchen Confidential", "Anthony Bourdain", 312, true)
 const bookThree = new Book("Manhattan Beach", "Jennifer Egan", 438, false)
 
-let myLibrary = [bookOne.bookInfo(), bookTwo.bookInfo(), bookThree.bookInfo()];
-/* in here we'll write a .forEach method to loop throgh myLibrary. */
-    /* thinking of how to do so. we'll use the .push() method to add the users input to the
-    end of our array. */
-    /* Now we need to figure out how we'll take in user input. for me, this may be easier to create
-    the button in html first. */
-    /* For right now, we are leaving this function. It will need to be worked out but it will work
-    as a placeholder for now. */
+let myLibrary = [];
 
-// Need to find way to make prompt in html to then create an object with info provided.
 
-function addBookToLibrary() {
-    return myLibrary.push();
+function addBookToLibrary(obj) {
+    return myLibrary.push(obj);
 }
 
-/*  let userInput = ["Thesis", "Rob Miller", 666, true]; 
- const userInputObject = new Book(userInput);
 
-function addBookToLibrary(userInput) {
-    return myLibrary.push(userInput);
-}
-console.log(addBookToLibrary(userInput));
-console.log(myLibrary); */
-/* ev.preventDefault(); */ // to stop the form from submitting. -We'll use this later for step 3.
-
-
-//Loop through myLibrary array and make them appear that way, not the main three.
-
-
-//This display below loops/adds items to array properly. Trying a for loop for better display
-/* let txt = "";
-for (let x in myLibrary) {
-    txt += myLibrary[x] + " ";
-  };
-
-  document.getElementById("library-container").innerHTML = txt; */
-
-//Oh hell ya
+//Need to reset the loop after each books submission (etchasketch project)
 const libraryContainer = document.getElementById('library-container');
 
 function createNewBookContainer() {
-  for(let i=0; i < myLibrary.length; i += 1){
+  
     let singleBook = document.createElement('div');
     singleBook.classList.add('bookListDiv');
-    singleBook.innerHTML = myLibrary[i];
+    singleBook.innerHTML = myLibrary;
     libraryContainer.appendChild(singleBook);
-  }
+  
 }
 
 
@@ -70,7 +41,11 @@ document.getElementById("new-book-btn").onclick = function() {
 document.getElementById("formElement").style.display = 'block';
 }
 
-//We now need this to return the book.Info for our object, not the new Book.
+//New book creator.
+function addBookToLibrary(obj) {
+    return myLibrary.push(obj);
+}
+
 document.addEventListener('submit', function(e) {
     e.preventDefault()
     const newBookForm = e.target
@@ -80,19 +55,9 @@ document.addEventListener('submit', function(e) {
     const newRead = newBookForm.read.value
         const newBookObj = new Book(newTitle, newAuthor, newPages, newRead)
 
-     return console.log(createNewBookContainer(myLibrary.push(newBookObj.bookInfo())));
+     return console.log(createNewBookContainer(addBookToLibrary(newBookObj.bookInfo())));
 }) 
 
 console.log(myLibrary);
 
-/* What are we trying to do.
-1.) Collect four answers from user input from our form.
-2.) Create an object using those four answers from the user submitted form.
-3.) Log that object and set it equal to userInput so that our addBookToLibrary function works.
-4.) An alternative to 3 would be to apply that function inside our form funciton, but try it without first. */
-/* 
-let userInput = new Book(
-    document.getElementById('new-title').value,
-    document.getElementById('new-author').value,
-    document.getElementById('new-pages').value,
-    document.getElementById('new-read').value, */
+//Need to make it so that the loop logs once. (create one book on each submission.)
